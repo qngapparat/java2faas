67ed2571cb9ee1f7e2d73166a38d61af7f9659e5
qngapparat <jakob.wallnoefer@outlook.com>
2020-02-28 08:49:26 +0100

    clean up transfs + notes

--- core/transformers/amazon.js
+++ core/transformers/amazon.js
@@ -1,10 +1,17 @@
 const path = require('path')
 const { runTransformers } = require('./common')
+/// ////////////////////////////////
+// READ THE NOTES
+/// ////////////////////////////////
 
-// TODO test this transformer (& gradle build succeeds) on ... build.gradle files
-
-// A transformator is a function that takes the user-input CLI args, and mutates a file content string
+// A transformator is a function that takes the user-input CLI args, and mutates a file content string (TLDR: it rewrites source code)
 const transformers = {
+  /**
+   * The transformator for the build.gradle file
+   * @param {string} cliArgs
+   * @param {string} prevCode Previous code (content) of the file
+   * @return {{code: string, path: string}} The new content of the file, and the path to save it to
+   */
   'build.gradle': function (cliArgs, prevCode) {
     // TODO make sure it produces sufficient build.gralde with empty prevCode
     let code = prevCode || '';
@@ -14,9 +21,7 @@ const transformers = {
     (() => {
       const applypluginjava = code.match(/apply\s*plugin\s*:\s*'java'/)
       if (!applypluginjava) {
-        code = `apply plugin: 'java'
-${code}
-        `
+        code = `apply plugin: 'java'\n${code}`
       }
     })();
 
@@ -24,14 +29,12 @@ ${code}
     (() => {
       let repositories = code.match(/repositories\s*{[^}]*}/)
       if (!repositories) {
-        code = `${code}\n repositories { 
-mavenCentral() 
-}`
-        return
-      }
-      repositories = repositories[0]
-      if (!repositories.includes('mavenCentral')) {
-        code = code.replace(/repositories\s*{/, 'repositories { mavenCentral()\n')
+        code = `${code}\n repositories { \nmavenCentral() \n}`
+      } else {
+        repositories = repositories[0]
+        if (!repositories.includes('mavenCentral')) {
+          code = code.replace(/repositories\s*{/, 'repositories { mavenCentral()\n')
+        }
       }
     })();
 
@@ -48,21 +51,20 @@ dependencies {
   compile 'com.google.code.gson:gson:2.6.2'
 }
       `
-        return
-      }
-
-      dependencies = dependencies[0]
-      // prepend (by replacing 'dependencies {' with 'dependencies { CODE' )
-      // Note the lack of closing } in the replace logic => we keep user-specified deps
-      if (!dependencies.includes('amazonaws:aws-lambda-java')) {
-        code = code.replace(/dependencies\s*{/, `dependencies { 
-  compile (
-    'com.amazonaws:aws-lambda-java-core:1.2.0',
-    'com.amazonaws:aws-lambda-java-events:2.2.7'
-  )
-  compile 'com.google.code.gson:gson:2.6.2'
-  `
-        )
+      } else {
+        dependencies = dependencies[0]
+        // prepend (by replacing 'dependencies {' with 'dependencies { CODE' )
+        // Note the lack of closing } in the replace logic => we keep user-specified deps
+        if (!dependencies.includes('amazonaws:aws-lambda-java')) {
+          code = code.replace(/dependencies\s*{/, `dependencies { 
+compile (
+  'com.amazonaws:aws-lambda-java-core:1.2.0',
+  'com.amazonaws:aws-lambda-java-events:2.2.7'
+)
+compile 'com.google.code.gson:gson:2.6.2'
+    `
+          )
+        }
       }
     })();
 
--- core/transformers/ibm.js
+++ core/transformers/ibm.js
@@ -1,76 +1,17 @@
 const path = require('path')
 const { runTransformers } = require('./common')
+/// ////////////////////////////////
+// READ THE NOTES
+/// ////////////////////////////////
 
-/**
- * Extract balanced-bracket groovy expressions like 'repositories { ...[{ ... }] ... }' from a string
- * @param {string} sel eg. 'repositories'
- * @param {string} code The whole code string
- */
-
-// TODO DO WE REALLY NEED THIS??
-// we just do
-// * new writes
-// * head inserts ('repos {' => 'repos { DATA' )
-// and when matching we already match repos\s*{
-// we dont care about how nested user expr may be
-// there will every only be one 'repos\s*{' anyway
-
-// TODO test this
-function groovyMatch (code, sel) {
-  // see if sel is even literally included
-  const lit = code.match(sel)
-  if (!lit) return lit
-  else {
-    // traverse everything behind eg. 'repositories' char by char and count opening/closing brackets
-
-    // we're interested in:
-    // a) what comes behind sel (sel could be 'repositores' and so on)
-    // b) if it's a balanced bracket expression
-    // IF IT IS => return it
-    // IF NOT => return ''
-    const suffixes = code.split(sel)
-    const r = suffixes.map(suf => {
-      let suffixExtract = ''
-      let pendingBrackets = 0
-      let allowTermination = false
-      for (const ch of suf) {
-        // first encountered ch MUST be whitespace, \n or {
-        // otherwise fail
-        if (allowTermination === false && !ch.match(/\s/) && ch !== '{') {
-          return ''
-        }
-        if (ch === '{') pendingBrackets += 1
-        if (ch === '}') pendingBrackets -= 1
-        suffixExtract += ch
-        //  eg. 'repositories' isn't valid, but 'repositories { ... }' is
-        // => allow termination after we encounter our first opening bracket
-        if (allowTermination === false && pendingBrackets > 0) allowTermination = true
-        if (pendingBrackets === 0 && allowTermination) break
-      }
-      // resulting extract should have at least one opening or closing bracket (failsafe)
-      if (!suffixExtract.match(/[{}]/)) return ''
-      return suffixExtract
-    })
-
-    // not interested in ''
-    return r.filter(ch => ch !== '')
-  }
-}
-
-/**
- * Replaces first occurence of fromStr with toStr, in code
- */
-function groovyReplaceOne (code, fromStr, toStr) {
-  let m = groovyMatch(code, fromStr)
-  if (!m) {
-    console.log(`Could not replace ${fromStr} => ${toStr} in ${code}`)
-    return
-  }
-  m = m[0]
-  return code.replace(m, toStr)
-}
-
+// A transformator is a function that takes the user-input CLI args, and mutates a file content string (TLDR: it rewrites source code)
 const transformers = {
+  /**
+   *
+   * @param {string} cliArgs
+   * @param {string} prevCode Previous code (content) of the file
+   * @return {{code: string, path: string}} The new content of the file, and the path to save it to
+   */
   'build.gradle': function (cliArgs, prevCode) {
     let code = prevCode || '';
     // TODO one closing curly bracket will fuck up regex (ie {{ => } <= })
@@ -79,22 +20,20 @@ const transformers = {
     (() => {
       const applypluginjava = code.match(/apply\s*plugin\s*:\s*'java'/)
       if (!applypluginjava) {
-        code = `apply plugin: 'java'
-${code}
-        `
+        code = `apply plugin: 'java'\n${code}`
       }
     })();
 
     // REPOSITORY FIELD
     (() => {
       let repositories = code.match(/repositories\s*{[^}]*}/)
+      // There is no repositories field yet - write repositories + mavenCentral
       if (!repositories) {
-        code = `${code}\n repositories { 
-mavenCentral() 
-}`
+        code = `${code}\nrepositories {\nmavenCentral()\n}`
         return
       }
       repositories = repositories[0]
+      // There is - write mavenCentral
       if (!repositories.includes('mavenCentral')) {
         code = code.replace(/repositories\s*{/, 'repositories { mavenCentral()\n')
       }
@@ -103,8 +42,9 @@ mavenCentral()
     // DEPENDENCY FIELD
     (() => {
       let dependencies = code.match(/dependencies\s*{[^}]*}/)
-      if (!dependencies) { // write 'dependencies { ... }'
-        code = `${code}\n 
+      // No dependencies field yet - write it
+      if (!dependencies) {
+        code = `${code}\n
 dependencies {
   provided 'com.google.code.gson:gson:2.6.2'
   compile 'com.google.zxing:core:3.3.0'
@@ -113,10 +53,11 @@ dependencies {
       `
         return
       }
-
+      // There is dependencies field already
       dependencies = dependencies[0]
-      // prepend (by replacing 'dependencies {' with 'dependencies { CODE' )
-      // Note the lack of closing } in the replace logic => we keep user-specified deps
+      // Explanation:
+      // prepend (by replacing 'dependencies {' with 'dependencies { CUSTOMCODE' )
+      // Note the lack of closing } in the replace logic => we keep user-specified deps that follow
       if (!dependencies.includes('com.google.zxing')) {
         code = code.replace(/dependencies\s*{/, `dependencies { 
   provided 'com.google.code.gson:gson:2.6.2'
@@ -134,7 +75,6 @@ configurations {
   provided
   compile.extendsFrom provided
 }
-
 ${code}
     `;
 
@@ -154,8 +94,7 @@ jar {
           `
       }
 
-      // If user wrote a jarbuilding task, replace it
-
+      // If user wrote a jar task, just replace it
       else {
         code = code.replace(/jar\s*{[^}]*}/, `jar {
   dependsOn configurations.runtime
@@ -184,46 +123,3 @@ function transformAll (cliArgs) {
 module.exports = {
   transformAll
 }
-
-const res = groovyMatch(`apply plugin: 'java'
-
-// tag::repositories[]
-repositories {
-    mavenCentral()
-}
-// end::repositories[]
-
-// tag::jar[]
-jar {
-    baseName = 'gs-gradle'
-    version =  '0.1.0'
-}
-// end::jar[]
-
-// tag::dependencies[]
-sourceCompatibility = 1.8
-targetCompatibility = 1.8
-
-dependencies { 
-  compile (
-    'com.amazonaws:aws-lambda-java-core:1.2.0',
-    'com.amazonaws:aws-lambda-java-events:2.2.7'
-  )
-  compile 'com.google.code.gson:gson:2.6.2'
-  
-    compile "joda-time:joda-time:2.2"
-    testCompile "junit:junit:4.12"
-}
-
-task buildZip(type: Zip) {
-  from compileJava
-  from processResources
-  into('lib') {
-      from configurations.runtimeClasspath
-  }
-}
-
-build.dependsOn buildZip
-    `, 'repositories')
-
-console.log(res)
