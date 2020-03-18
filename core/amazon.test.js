1ffd163f86792b6d965097120f9f63a389995990
qngapparat <jakob.wallnoefer@outlook.com>
2020-03-18 10:12:10 +0100

    top-level tests: => WIN/Mac compatible paths

--- core/amazon.test.js
+++ core/amazon.test.js
@@ -2,6 +2,7 @@ const { amazon } = require('./amazon')
 const uuidv4 = require('uuid').v4
 const path = require('path')
 const fs = require('fs')
+const os = require('os')
 const { execSync } = require('child_process')
 let runuuid
 
@@ -9,11 +10,11 @@ describe('Top-level Amazon', () => {
   describe('default Java package', () => {
     beforeEach(() => {
       runuuid = uuidv4()
-      fs.mkdirSync(`/tmp/${runuuid}`)
-      fs.mkdirSync(`/tmp/${runuuid}/src`)
-      fs.mkdirSync(`/tmp/${runuuid}/src/main`)
-      fs.mkdirSync(`/tmp/${runuuid}/src/main/java`)
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Hello.java`, `
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java'))
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Hello.java'), `
     public class Hello {
       public Response hello(Request inp) {
         String greetingString = String.format("Hello %s %s.", inp.firstName, inp.lastName);
@@ -21,7 +22,7 @@ describe('Top-level Amazon', () => {
       }
     }
     `)
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Request.java`, ` 
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Request.java'), ` 
   public class Request {
     String firstName;
     String lastName;
@@ -52,7 +53,7 @@ describe('Top-level Amazon', () => {
   }
     `)
 
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Response.java`, ` 
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Response.java'), ` 
   public class Response {
     String greetings;
   
@@ -75,62 +76,63 @@ describe('Top-level Amazon', () => {
     })
 
     afterEach(() => {
-    // it suffices to just forget runuuid, will be wiped anyway
+      // it suffices to just forget runuuid, will be wiped anyway
       runuuid = null
     })
 
     test('produces buildable proj | without explicit Req/Res', () => {
       const mockCliArgs = {
-        '--path': `/tmp/${runuuid}`,
-        '--entry-file': `/tmp/${runuuid}/src/main/java/Hello.java`,
+        '--path': path.join(os.tmpdir(), runuuid),
+        '--entry-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Hello.java'),
         '--entry-method': 'hello',
         '--name': 'cloudfname123'
       }
 
       amazon(mockCliArgs)
 
-      const fn = jest.fn(() => execSync(`gradle build -b /tmp/${runuuid}/amazon/build.gradle`))
+      path.join(os.tmpdir(), runuuid, 'amazon', 'build.gradle')
+
+      const fn = jest.fn(() => execSync(`gradle build -b ${path.join(os.tmpdir(), runuuid, 'amazon', 'build.gradle')}`))
       fn()
       expect(fn).toHaveReturned()
     })
 
     test('produces buildable proj | with explicit Req/Res', () => {
       const mockCliArgs = {
-        '--path': `/tmp/${runuuid}`,
-        '--entry-file': `/tmp/${runuuid}/src/main/java/Hello.java`,
-        '--request-file': `/tmp/${runuuid}/src/main/java/Req.java`,
-        '--response-file': `/tmp/${runuuid}/src/main/java/Res.java`,
+        '--path': path.join(os.tmpdir(), runuuid),
+        '--entry-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Hello.java'),
+        '--request-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Req.java'),
+        '--response-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Res.java'),
         '--entry-method': 'hello',
         '--name': 'cloudfname123'
       }
 
       // Rename Request.java => Req.java
       // Rename Response.java => Res.java for testing purposes
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Req.java`,
-        fs.readFileSync(`/tmp/${runuuid}/src/main/java/Request.java`, { encoding: 'utf8' })
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Req.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Request.java'), { encoding: 'utf8' })
           .replace(/Request/g, 'Req')
       )
 
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Res.java`,
-        fs.readFileSync(`/tmp/${runuuid}/src/main/java/Response.java`, { encoding: 'utf8' })
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Res.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Response.java'), { encoding: 'utf8' })
           .replace(/Response/g, 'Res')
       )
       // Rename usages of Request, Response
       const entryFname = mockCliArgs['--entry-file'].split(path.sep).slice(-1)[0]
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/${entryFname}`,
-        fs.readFileSync(`/tmp/${runuuid}/src/main/java/${entryFname}`, { encoding: 'utf8' })
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', entryFname),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', entryFname), { encoding: 'utf8' })
           .replace(/Response/g, 'Res')
           .replace(/Request/g, 'Req')
       )
 
       // Remove moved files
-      fs.unlinkSync(`/tmp/${runuuid}/src/main/java/Response.java`)
-      fs.unlinkSync(`/tmp/${runuuid}/src/main/java/Request.java`)
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Request.java'))
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Response.java'))
 
       // See if it still works
       amazon(mockCliArgs)
-
-      const fn = jest.fn(() => execSync(`gradle build -b /tmp/${runuuid}/amazon/build.gradle`))
+      const fn = jest.fn(() => execSync(`gradle build -b ${path.join(os.tmpdir(), runuuid, 'amazon', 'build.gradle')}`))
       fn()
       expect(fn).toHaveReturned()
     })
@@ -141,12 +143,12 @@ describe('Top-level Amazon', () => {
   describe('example Package', () => {
     beforeEach(() => {
       runuuid = uuidv4()
-      fs.mkdirSync(`/tmp/${runuuid}`)
-      fs.mkdirSync(`/tmp/${runuuid}/src`)
-      fs.mkdirSync(`/tmp/${runuuid}/src/main`)
-      fs.mkdirSync(`/tmp/${runuuid}/src/main/java`)
-      fs.mkdirSync(`/tmp/${runuuid}/src/main/java/example`)
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Hello.java`, `
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example'))
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Hello.java'), `
     package example;
     public class Hello {
       public Response hello(Request inp) {
@@ -155,7 +157,7 @@ describe('Top-level Amazon', () => {
       }
     }
     `)
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Request.java`, `
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Request.java'), `
   package example; 
   public class Request {
     String firstName;
@@ -187,7 +189,7 @@ describe('Top-level Amazon', () => {
   }
     `)
 
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Response.java`, ` 
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Response.java'), ` 
   package example;
   public class Response {
     String greetings;
@@ -211,46 +213,46 @@ describe('Top-level Amazon', () => {
     })
 
     afterEach(() => {
-    // it suffices to just forget runuuid, will be wiped anyway
+      // it suffices to just forget runuuid, will be wiped anyway
       runuuid = null
     })
     test('produces buildable proj | with explicit Req/Res | example package', () => {
       const mockCliArgs = {
-        '--path': `/tmp/${runuuid}`,
-        '--entry-file': `/tmp/${runuuid}/src/main/java/example/Hello.java`,
-        '--request-file': `/tmp/${runuuid}/src/main/java/example/Req.java`,
-        '--response-file': `/tmp/${runuuid}/src/main/java/example/Res.java`,
+        '--path': path.join(os.tmpdir(), runuuid),
+        '--entry-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Hello.java'),
+        '--request-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Req.java'),
+        '--response-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Res.java'),
         '--entry-method': 'hello',
         '--name': 'cloudfname123'
       }
 
       // Rename Request.java => Req.java
       // Rename Response.java => Res.java for testing purposes
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Req.java`,
-        fs.readFileSync(`/tmp/${runuuid}/src/main/java/example/Request.java`, { encoding: 'utf8' })
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Req.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Request.java'), { encoding: 'utf8' })
           .replace(/Request/g, 'Req')
       )
 
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Res.java`,
-        fs.readFileSync(`/tmp/${runuuid}/src/main/java/example/Response.java`, { encoding: 'utf8' })
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Res.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Response.java'), { encoding: 'utf8' })
           .replace(/Response/g, 'Res')
       )
       // Rename usages of Request, Response
       const entryFname = mockCliArgs['--entry-file'].split(path.sep).slice(-1)[0]
-      fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/${entryFname}`,
-        fs.readFileSync(`/tmp/${runuuid}/src/main/java/example/${entryFname}`, { encoding: 'utf8' })
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', entryFname),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', entryFname), { encoding: 'utf8' })
           .replace(/Response/g, 'Res')
           .replace(/Request/g, 'Req')
       )
 
       // Remove moved files
-      fs.unlinkSync(`/tmp/${runuuid}/src/main/java/example/Response.java`)
-      fs.unlinkSync(`/tmp/${runuuid}/src/main/java/example/Request.java`)
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Response.java'))
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Request.java'))
 
       // See if it still works
       amazon(mockCliArgs)
 
-      const fn = jest.fn(() => execSync(`gradle build -b /tmp/${runuuid}/amazon/build.gradle`))
+      const fn = jest.fn(() => execSync(`gradle build -b ${path.join(os.tmpdir(), runuuid, 'amazon', 'build.gradle')}`))
       fn()
       expect(fn).toHaveReturned()
     })
--- core/ibm.test.js
+++ core/ibm.test.js
@@ -2,253 +2,259 @@ const { ibm } = require('./ibm')
 const uuidv4 = require('uuid').v4
 const path = require('path')
 const fs = require('fs')
+const os = require('os')
 const { execSync } = require('child_process')
 let runuuid
 
-describe('top-level ibm comp | default package', () => {
-  beforeEach(() => {
-    runuuid = uuidv4()
-    fs.mkdirSync(`/tmp/${runuuid}`)
-    fs.mkdirSync(`/tmp/${runuuid}/src`)
-    fs.mkdirSync(`/tmp/${runuuid}/src/main`)
-    fs.mkdirSync(`/tmp/${runuuid}/src/main/java`)
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Hello.java`, `
-  public class Hello {
-    public Response hello(Request inp) {
-      String greetingString = String.format("Hello %s %s.", inp.firstName, inp.lastName);
-      return new Response(greetingString);
+describe('Top-level Ibm', () => {
+  describe('default Java package', () => {
+    beforeEach(() => {
+      runuuid = uuidv4()
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java'))
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Hello.java'), `
+    public class Hello {
+      public Response hello(Request inp) {
+        String greetingString = String.format("Hello %s %s.", inp.firstName, inp.lastName);
+        return new Response(greetingString);
+      }
+    }
+    `)
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Request.java'), ` 
+  public class Request {
+    String firstName;
+    String lastName;
+  
+    public String getFirstName() {
+        return firstName;
+    }
+  
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+  
+    public String getLastName() {
+        return lastName;
+    }
+  
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+  
+    public Request(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+  
+    public Request() {
     }
   }
-  `)
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Request.java`, ` 
-public class Request {
-  String firstName;
-  String lastName;
+    `)
 
-  public String getFirstName() {
-      return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-      this.firstName = firstName;
-  }
-
-  public String getLastName() {
-      return lastName;
-  }
-
-  public void setLastName(String lastName) {
-      this.lastName = lastName;
-  }
-
-  public Request(String firstName, String lastName) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-  }
-
-  public Request() {
-  }
-}
-  `)
-
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Response.java`, ` 
-public class Response {
-  String greetings;
-
-  public String getGreetings() {
-      return greetings;
-  }
-
-  public void setGreetings(String greetings) {
-      this.greetings = greetings;
-  }
-
-  public Response(String greetings) {
-      this.greetings = greetings;
-  }
-
-  public Response() {
-  }
-}
-  `)
-  })
-
-  afterEach(() => {
-  // it suffices to just forget runuuid, will be wiped anyway
-    runuuid = null
-  })
-
-  test('produces buildable proj | without explicit Req/Res', () => {
-    const mockCliArgs = {
-      '--path': `/tmp/${runuuid}`,
-      '--entry-file': `/tmp/${runuuid}/src/main/java/Hello.java`,
-      '--entry-method': 'hello',
-      '--name': 'cloudfname123'
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Response.java'), ` 
+  public class Response {
+    String greetings;
+  
+    public String getGreetings() {
+        return greetings;
     }
-
-    ibm(mockCliArgs)
-
-    const fn = jest.fn(() => execSync(`gradle build -b /tmp/${runuuid}/ibm/build.gradle`))
-    fn()
-    expect(fn).toHaveReturned()
-  })
-
-  test('produces buildable proj | with explicit Req/Res', () => {
-    const mockCliArgs = {
-      '--path': `/tmp/${runuuid}`,
-      '--entry-file': `/tmp/${runuuid}/src/main/java/Hello.java`,
-      '--request-file': `/tmp/${runuuid}/src/main/java/Req.java`,
-      '--response-file': `/tmp/${runuuid}/src/main/java/Res.java`,
-      '--entry-method': 'hello',
-      '--name': 'cloudfname123'
+  
+    public void setGreetings(String greetings) {
+        this.greetings = greetings;
     }
+  
+    public Response(String greetings) {
+        this.greetings = greetings;
+    }
+  
+    public Response() {
+    }
+  }
+    `)
+    })
 
-    // Rename Request.java => Req.java
-    // Rename Response.java => Res.java for testing purposes
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Req.java`,
-      fs.readFileSync(`/tmp/${runuuid}/src/main/java/Request.java`, { encoding: 'utf8' })
-        .replace(/Request/g, 'Req')
-    )
+    afterEach(() => {
+      // it suffices to just forget runuuid, will be wiped anyway
+      runuuid = null
+    })
 
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/Res.java`,
-      fs.readFileSync(`/tmp/${runuuid}/src/main/java/Response.java`, { encoding: 'utf8' })
-        .replace(/Response/g, 'Res')
-    )
-    // Rename usages of Request, Response
-    const entryFname = mockCliArgs['--entry-file'].split(path.sep).slice(-1)[0]
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/${entryFname}`,
-      fs.readFileSync(`/tmp/${runuuid}/src/main/java/${entryFname}`, { encoding: 'utf8' })
-        .replace(/Response/g, 'Res')
-        .replace(/Request/g, 'Req')
-    )
+    test('produces buildable proj | without explicit Req/Res', () => {
+      const mockCliArgs = {
+        '--path': path.join(os.tmpdir(), runuuid),
+        '--entry-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Hello.java'),
+        '--entry-method': 'hello',
+        '--name': 'cloudfname123'
+      }
 
-    // Remove moved files
-    fs.unlinkSync(`/tmp/${runuuid}/src/main/java/Response.java`)
-    fs.unlinkSync(`/tmp/${runuuid}/src/main/java/Request.java`)
+      ibm(mockCliArgs)
 
-    // See if it still works
-    ibm(mockCliArgs)
+      path.join(os.tmpdir(), runuuid, 'ibm', 'build.gradle')
 
-    const fn = jest.fn(() => execSync(`gradle build -b /tmp/${runuuid}/ibm/build.gradle`))
-    fn()
-    expect(fn).toHaveReturned()
+      const fn = jest.fn(() => execSync(`gradle build -b ${path.join(os.tmpdir(), runuuid, 'ibm', 'build.gradle')}`))
+      fn()
+      expect(fn).toHaveReturned()
+    })
+
+    test('produces buildable proj | with explicit Req/Res', () => {
+      const mockCliArgs = {
+        '--path': path.join(os.tmpdir(), runuuid),
+        '--entry-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Hello.java'),
+        '--request-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Req.java'),
+        '--response-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Res.java'),
+        '--entry-method': 'hello',
+        '--name': 'cloudfname123'
+      }
+
+      // Rename Request.java => Req.java
+      // Rename Response.java => Res.java for testing purposes
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Req.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Request.java'), { encoding: 'utf8' })
+          .replace(/Request/g, 'Req')
+      )
+
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Res.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Response.java'), { encoding: 'utf8' })
+          .replace(/Response/g, 'Res')
+      )
+      // Rename usages of Request, Response
+      const entryFname = mockCliArgs['--entry-file'].split(path.sep).slice(-1)[0]
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', entryFname),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', entryFname), { encoding: 'utf8' })
+          .replace(/Response/g, 'Res')
+          .replace(/Request/g, 'Req')
+      )
+
+      // Remove moved files
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Request.java'))
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'Response.java'))
+
+      // See if it still works
+      ibm(mockCliArgs)
+      const fn = jest.fn(() => execSync(`gradle build -b ${path.join(os.tmpdir(), runuuid, 'ibm', 'build.gradle')}`))
+      fn()
+      expect(fn).toHaveReturned()
+    })
   })
 })
 
-describe('top-level ibm comp | example package', () => {
-  beforeEach(() => {
-    runuuid = uuidv4()
-    fs.mkdirSync(`/tmp/${runuuid}`)
-    fs.mkdirSync(`/tmp/${runuuid}/src`)
-    fs.mkdirSync(`/tmp/${runuuid}/src/main`)
-    fs.mkdirSync(`/tmp/${runuuid}/src/main/java`)
-    fs.mkdirSync(`/tmp/${runuuid}/src/main/java/example`)
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Hello.java`, `
+describe('Top-level Ibm', () => {
+  describe('example Package', () => {
+    beforeEach(() => {
+      runuuid = uuidv4()
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java'))
+      fs.mkdirSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example'))
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Hello.java'), `
+    package example;
+    public class Hello {
+      public Response hello(Request inp) {
+        String greetingString = String.format("Hello %s %s.", inp.firstName, inp.lastName);
+        return new Response(greetingString);
+      }
+    }
+    `)
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Request.java'), `
+  package example; 
+  public class Request {
+    String firstName;
+    String lastName;
+  
+    public String getFirstName() {
+        return firstName;
+    }
+  
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+  
+    public String getLastName() {
+        return lastName;
+    }
+  
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+  
+    public Request(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+  
+    public Request() {
+    }
+  }
+    `)
+
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Response.java'), ` 
   package example;
-  public class Hello {
-    public Response hello(Request inp) {
-      String greetingString = String.format("Hello %s %s.", inp.firstName, inp.lastName);
-      return new Response(greetingString);
+  public class Response {
+    String greetings;
+  
+    public String getGreetings() {
+        return greetings;
+    }
+  
+    public void setGreetings(String greetings) {
+        this.greetings = greetings;
+    }
+  
+    public Response(String greetings) {
+        this.greetings = greetings;
+    }
+  
+    public Response() {
     }
   }
-  `)
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Request.java`, `
-package example; 
-public class Request {
-  String firstName;
-  String lastName;
+    `)
+    })
 
-  public String getFirstName() {
-      return firstName;
-  }
+    afterEach(() => {
+      // it suffices to just forget runuuid, will be wiped anyway
+      runuuid = null
+    })
+    test('produces buildable proj | with explicit Req/Res | example package', () => {
+      const mockCliArgs = {
+        '--path': path.join(os.tmpdir(), runuuid),
+        '--entry-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Hello.java'),
+        '--request-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Req.java'),
+        '--response-file': path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Res.java'),
+        '--entry-method': 'hello',
+        '--name': 'cloudfname123'
+      }
 
-  public void setFirstName(String firstName) {
-      this.firstName = firstName;
-  }
+      // Rename Request.java => Req.java
+      // Rename Response.java => Res.java for testing purposes
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Req.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Request.java'), { encoding: 'utf8' })
+          .replace(/Request/g, 'Req')
+      )
 
-  public String getLastName() {
-      return lastName;
-  }
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Res.java'),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Response.java'), { encoding: 'utf8' })
+          .replace(/Response/g, 'Res')
+      )
+      // Rename usages of Request, Response
+      const entryFname = mockCliArgs['--entry-file'].split(path.sep).slice(-1)[0]
+      fs.writeFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', entryFname),
+        fs.readFileSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', entryFname), { encoding: 'utf8' })
+          .replace(/Response/g, 'Res')
+          .replace(/Request/g, 'Req')
+      )
 
-  public void setLastName(String lastName) {
-      this.lastName = lastName;
-  }
+      // Remove moved files
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Response.java'))
+      fs.unlinkSync(path.join(os.tmpdir(), runuuid, 'src', 'main', 'java', 'example', 'Request.java'))
 
-  public Request(String firstName, String lastName) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-  }
+      // See if it still works
+      ibm(mockCliArgs)
 
-  public Request() {
-  }
-}
-  `)
-
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Response.java`, ` 
-package example;
-public class Response {
-  String greetings;
-
-  public String getGreetings() {
-      return greetings;
-  }
-
-  public void setGreetings(String greetings) {
-      this.greetings = greetings;
-  }
-
-  public Response(String greetings) {
-      this.greetings = greetings;
-  }
-
-  public Response() {
-  }
-}
-  `)
-  })
-
-  afterEach(() => {
-  // it suffices to just forget runuuid, will be wiped anyway
-    runuuid = null
-  })
-  test('produces buildable proj | with explicit Req/Res | example package', () => {
-    const mockCliArgs = {
-      '--path': `/tmp/${runuuid}`,
-      '--entry-file': `/tmp/${runuuid}/src/main/java/example/Hello.java`,
-      '--request-file': `/tmp/${runuuid}/src/main/java/example/Req.java`,
-      '--response-file': `/tmp/${runuuid}/src/main/java/example/Res.java`,
-      '--entry-method': 'hello',
-      '--name': 'cloudfname123'
-    }
-
-    // Rename Request.java => Req.java
-    // Rename Response.java => Res.java for testing purposes
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Req.java`,
-      fs.readFileSync(`/tmp/${runuuid}/src/main/java/example/Request.java`, { encoding: 'utf8' })
-        .replace(/Request/g, 'Req')
-    )
-
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/Res.java`,
-      fs.readFileSync(`/tmp/${runuuid}/src/main/java/example/Response.java`, { encoding: 'utf8' })
-        .replace(/Response/g, 'Res')
-    )
-    // Rename usages of Request, Response
-    const entryFname = mockCliArgs['--entry-file'].split(path.sep).slice(-1)[0]
-    fs.writeFileSync(`/tmp/${runuuid}/src/main/java/example/${entryFname}`,
-      fs.readFileSync(`/tmp/${runuuid}/src/main/java/example/${entryFname}`, { encoding: 'utf8' })
-        .replace(/Response/g, 'Res')
-        .replace(/Request/g, 'Req')
-    )
-
-    // Remove moved files
-    fs.unlinkSync(`/tmp/${runuuid}/src/main/java/example/Response.java`)
-    fs.unlinkSync(`/tmp/${runuuid}/src/main/java/example/Request.java`)
-
-    // See if it still works
-    ibm(mockCliArgs)
-
-    const fn = jest.fn(() => execSync(`gradle build -b /tmp/${runuuid}/ibm/build.gradle`))
-    fn()
-    expect(fn).toHaveReturned()
+      const fn = jest.fn(() => execSync(`gradle build -b ${path.join(os.tmpdir(), runuuid, 'ibm', 'build.gradle')}`))
+      fn()
+      expect(fn).toHaveReturned()
+    })
   })
 })
